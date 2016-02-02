61c78f1a79be7e91ef831f6a2c0419d49a64baff
Adam <adam@snikrepmada.com>
2016-02-01 20:39:53 -0500

    Added debug files.

---
+++ camera2640/Src/debug.c
@@ -0,0 +1,16 @@
+#include "debug.c"
+
+struct __FILE
+{
+  int handle;
+};
+FILE __stdout;
+FILE __stdin;
+
+int fputc(int ch, FILE *f) {
+  if (DEMCR & TRCENA) {
+    while (ITM_Port32(0) == 0);
+    ITM_Port8(0) = ch;
+  }
+  return(ch);
+}
